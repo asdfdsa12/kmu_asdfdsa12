bc05079ddac3bb8cd0fcd6bf5e8914319f61ee5c
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-03-10 14:36:54 +0900

    Update work(1)_2.c

--- work(1)_2.c
+++ work(1)_2.c
@@ -1,16 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main() {
 	printf("입력값: ");
-	float a;
-	scanf("%f", &a);
-	int b=a/1;
-	for(int i=8;i>0;i--){
-	    if((b>>i)&1==1)
-	        printf("%d",1);
-	    else
-	        printf("%d",0);
+	double a;
+	scanf("%lf", &a);
+	int b=a;
+	if(a<0){
+	    printf("%d",1);
+	    for(int i=6;i>=0;i--){
+	        int q=pow(2,i);
+	        if(b/q==-1){
+	            printf("%d",0);
+	            b/=q;
+	        }
+	        else
+	            printf("%d",1);
+	    }
 	}
-	return 0;
+	else{
+    	for(int i=7;i>=0;i--){
+    	    int q=pow(2,i);
+    	    if(b/q==1){
+    	        printf("%d",1);
+    	        b/=q;
+    	    }
+    	    else
+    	        printf("%d",0);
+        }
+	}
+    return 0;
 }
