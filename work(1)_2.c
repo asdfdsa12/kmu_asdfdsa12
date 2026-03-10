f7f3c803e1360a9c58d98e95fe1b446be5fce67f
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-03-10 15:14:51 +0900

    Update work(1)_2.c

--- work(1)_2.c
+++ work(1)_2.c
@@ -1,48 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 
-int main() {
-	printf("입력값: ");
-	double a;
-	scanf("%lf", &a);
-	int b=a;
-	int q;
-	double p=abs(a*10-b*10);
-	printf("%d %lf\n",p,p);
-	if(a<0){
-	    printf("%d",1);
-	    for(int i=6;i>=0;i--){
-	        q=pow(2,i);
-	        if(b/q==-1){
-	            printf("%d",0);
-	            b/=q;
-	        }
-	        else
-	            printf("%d",1);
-	    }
-	}
-	else{
-    	for(int i=7;i>=0;i--){
-    	    q=pow(2,i);
-    	    if(b/q==1){
-    	        printf("%d",1);
-    	        b/=q;
-    	    }
-    	    else
-    	        printf("%d",0);
-        }
-	}
-	printf(".");
-	for(int i=7;i>=0;i--){
-	    p*=2;
-	    if(p>=10){
-	        printf("%d",1);
-	        p-=10;
-	    }
-	    else
-	        printf("%d",0);
-	}
+int main(){
+    printf("입력값: ");
+    double a;
+    scanf("%lf",&a);
+    int q=a*pow(2,8);
+    for(int i=15;i>=0;i--){
+        if((q>>i)&1)
+            printf("%d",1);
+        else
+            printf("%d",0);
+        if(i==8)
+            printf(".");
+    }
     return 0;
 }
