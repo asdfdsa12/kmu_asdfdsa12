9b32c23dd89f26bc6c9befcbeab268ac88dfca9e
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-03-12 14:43:37 +0900

    Update work(2)_1.c

--- work(2)_1.c
+++ work(2)_1.c
@@ -1,33 +1,46 @@
-#define _CRT_SECURE_NO_WARNIGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int main(){
     double a;
     scanf("%lf",&a);
-    int b=a;
-    int c=0;
-    while(b!=1){
-        b=b>>1;
-        c++;
+    if(a==0){
+        printf("0/0000 0000/0000 0000 0000 0000 0000 000");
+        return 0;
     }
-    int d=c+127;
-    for(int i=8;i>=0;i--){
-        if((d>>i)&1)
+    int b=abs(a);
+    int exponent=0;
+    if(b!=0){
+        while(b!=1){
+            b=b>>1;
+            exponent++;
+        }
+    }
+    else if(b==0&&a<0)
+        exponent--;
+    int bias=exponent+127;
+    if(a<0){
+        printf("1/");
+        a=-a;
+    }
+    else
+        printf("0/");
+    for(int i=7;i>=0;i--){
+        if((bias>>i)&1)
             printf("1");
         else
             printf("0");
-        if(i==8)
-            printf("/");
-        else if(i==4)
+        if(i==4)
             printf(" ");
     }
-    int e=a*pow(2,21-c);
+    int e=a*pow(2,23);
     printf("/");
     int f=0;
     for(int i=22;i>=0;i--){
         f++;
-        if((e>>i-c)&1)
+        if((e>>i+exponent)&1)
             printf("1");
         else
             printf("0");
