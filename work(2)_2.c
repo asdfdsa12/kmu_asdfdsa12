b7ba4851902fb9b1c757c5c4307fb0c945b96486
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-03-12 11:22:38 +0900

    Update work(2)_2.c

--- work(2)_2.c
+++ work(2)_2.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
     char a[100];
-    scanf("%s",a);
+    scanf("%[^\n]",a);
     int i=0;
     while(1){
         if(a[i]>=97)
             printf("%c",a[i]-32);
-        else
+        else if(a[i]>=65)
             printf("%c",a[i]+32);
+        else
+            printf("%c",a[i]);
         i++;
         if(a[i]=='\0')
             break;
