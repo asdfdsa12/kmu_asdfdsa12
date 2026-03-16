b284dc42d20c6b57860f34d1802a8599af0abe2a
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-03-17 08:54:19 +0900

    Update work(2)_2.c

--- work(2)_2.c
+++ work(2)_2.c
@@ -2,17 +2,17 @@
 
 int main() {
     char a[100];
-    fgets(a,sizeof(a),stdin); //VS에서는 gets_s(a,sizeof(a)); 가능
-    int i=0;
-    while(1){
-        if(a[i]>=97&&a[i]<=122)
-            printf("%c",a[i]-32);
-        else if(a[i]>=65&&a[i]<=90)
-            printf("%c",a[i]+32);
+    gets_s(a, sizeof(a));
+    int i = 0;
+    while (1) {
+        if (a[i] >= 97 && a[i] <= 122)
+            printf("%c", a[i] - 32);
+        else if (a[i] >= 65 && a[i] <= 90)
+            printf("%c", a[i] + 32);
         else
-            printf("%c",a[i]);
+            printf("%c", a[i]);
         i++;
-        if(a[i]=='\0')
+        if (a[i] == '\0')
             break;
     }
     return 0;
