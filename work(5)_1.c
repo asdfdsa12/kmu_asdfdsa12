a2f2255a5b54d84568e681194fd40a1435f92510
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-03-31 11:02:52 +0900

    Update work(5)_1.c

--- work(5)_1.c
+++ work(5)_1.c
@@ -1,41 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define N 100
 
 int main() {
-	int n;
 	int sum = 0;
 	int max = 0;
-	int min = 0;
-	int* q;
-
-	scanf("%d", &n);
-
-	q = (int*)malloc(sizeof(int) * n);
+	int q[N];
+	int min = N;
 
 	srand(time(NULL));
 
-	printf("\n처음에서 끝:\n");
+	printf("처음에서 끝:\n");
 
-	for (int i = 0; i < n; i++) {
-		q[i] = rand()%(n+1);
+	for (int i = 0; i < N; i++) {
+		q[i] = rand()%(N+1);
 		printf("%d ", q[i]);
 		sum += q[i];
 		if (q[i] > max)
 			max = q[i];
+		if (q[i] < min)
+		    min = q[i];
 	}
 
 	printf("\n\n끝에서 처음:\n");
 
-	min = *q;
-
-	for (int i = n-1; i >= 0; i--) {
+	for (int i = N-1; i >= 0; i--) {
 		printf("%d ", q[i]);
-		if (min > q[i])
-			min = q[i];
 	}
 
 	printf("\n\n총합: %d, 최대값: %d, 최소값: %d", sum,max,min);
-	free(q);
 }
