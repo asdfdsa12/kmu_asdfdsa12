d545da7f5c8ec8124b8255fd1996438f3ebd0e08
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-04-16 11:58:42 +0900

    Add files via upload

---
+++ work(10)/work(10)_1/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arrayList.h"
+
+int main() {
+	arrayList* myArrayList;
+	myArrayList = createArrayList(0);
+
+	if (sizeArrayList(myArrayList) == 0) {
+		printf("Create ok\n");
+	}
+
+	insertArrayList(myArrayList, 0, 10);
+	insertArrayList(myArrayList, 1, 20);
+	insertArrayList(myArrayList, 2, -30);
+	insertArrayList(myArrayList, 0, 5);
+
+	printArrayList(myArrayList);
+
+	deleteArrayList(myArrayList, 0);
+	deleteArrayList(myArrayList, 1);
+
+	printArrayList(myArrayList);
+	
+	replaceItemArrayList(myArrayList, 0, 50);
+	
+	printArrayList(myArrayList);
+	
+	initArrayList(myArrayList);
+	printArrayList(myArrayList);
+	
+	destroyArrayList(myArrayList);
+	
+	return 0;
+}
