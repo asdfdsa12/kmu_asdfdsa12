605acb1822926e6de2c7d8fb219d889e7d607d69
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-04-21 12:14:52 +0900

    Add files via upload

---
+++ work(11)/main.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "polynomial.h"
+
+int main() {
+	polynomial* p1 = createPolynomial(100);
+	int co,ex;
+	char plus;
+	while(1){
+	    scanf("%dX^%d%c",&co,&ex,&plus);
+	    addItemPolynomial(p1, (elementType){co,ex});
+	    if(plus!='+')
+	        break;
+	}
+	
+	printPolynomial(p1);
+
+	polynomial* p2 = createPolynomial(100);
+
+	while(1){
+	    scanf("%dX^%d%c",&co,&ex,&plus);
+	    addItemPolynomial(p2, (elementType){co,ex});
+	    if(plus!='+')
+	        break;
+	}
+
+	printPolynomial(p2);
+
+	polynomial* p3 = timePolynomial(p1, p2);
+
+	printPolynomial(p3);
+}
