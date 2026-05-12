47736b8cf4d7ae7d73788a336e9062f63bec311f
asdfdsa12 <151939467+asdfdsa12@users.noreply.github.com>
2026-05-12 10:12:08 +0900

    Update LinkedList.c

--- work(12)/LinkedList.c
+++ work(12)/LinkedList.c
@@ -1,3 +1,76 @@
+#include "LinkedList.h"
+
+LinkedList* createLinkedList() {
+	struct linkedList* r;
+	r = (struct linkedList*)malloc(sizeof(struct linkedList));
+
+	r->head = (struct pointType*)NULL;
+	r->size = 0;
+
+	return r;
+}
+
+int destroyLinkedList(LinkedList* li) {
+	struct pointType* nptr, * mptr;
+	for (nptr = li->head; nptr != NULL; nptr = mptr) {
+		mptr = nptr->next;
+		free(nptr);
+	}
+
+	free(li);
+
+	return 1;
+}
+
+int isEmptyLinkedList(LinkedList* li) {
+	return (li->size == 0);
+}
+
+int sizeLinkedList(LinkedList* li) {
+	return li->size;
+}
+
+LinkedList* insertFirstLinkedList(LinkedList* li, PointType item) {
+	PointType* ptr = (PointType*)malloc(sizeof(PointType));
+	*ptr = item;
+
+	ptr->next = li->head;
+	li->head = ptr;
+	li->size++;
+
+	return li;
+}
+
+int printLinkedList(LinkedList* li) {
+	PointType* current = li->head;
+	printf("LinkedList:\n");
+
+	for (int i = 0; i < li->size; i++) {
+		printf("[%d] x:%d, y:%d, next:%x\n", 
+			i, current->x, current->y, current->next);
+
+		current = current->next;
+	}
+}
+
+LinkedList* insertLastLinkedList(LinkedList* li, PointType item) {
+	PointType* ptr = li->head;
+
+	while (ptr->next != NULL) {
+		ptr = ptr->next;
+	}
+
+	PointType* nPtr = (PointType*)malloc(sizeof(PointType));
+	*nPtr = item;
+	nPtr->next = NULL;
+
+	ptr->next = nPtr;
+
+	li->size++;
+
+	return li;
+}
+
 LinkedList* deleteAtLinkedList(LinkedList* li, int at) {
 	if (at >= 0 && at < li->size) {
 		PointType* nPtr = li->head;
